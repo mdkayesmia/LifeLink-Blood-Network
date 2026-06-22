0500b78d6968aa6a6e51849b8a9b7a2b5b453ea6
Md Kayes Mia <mdkayesmia01@gmail.com>
2026-06-22 14:04:51 +0600

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -43,7 +43,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://6a38eab778fb99f4bb253e01--chic-bavarois-4f4841.netlify.app/auth/callback",
       },
     });
 
